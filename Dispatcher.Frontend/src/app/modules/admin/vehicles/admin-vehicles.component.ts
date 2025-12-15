c7f09992546806ca79fdf5dda6109160cc98779f
Haris Saric <harissaric55@gmail.com>
2025-12-15 16:57:47 +0100

    refactor(admin-routes): update sidebar navigation routes

---
+++ Dispatcher.Frontend/src/app/modules/admin/vehicles/admin-vehicles.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-vehicles',
+  standalone: false,
+  templateUrl: './admin-vehicles.component.html',
+  styleUrl: './admin-vehicles.component.scss',
+})
+export class AdminVehiclesComponent {
+
+}
