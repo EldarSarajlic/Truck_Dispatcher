f9a3f4a241a8f0e5965bdde7b207becda909b1e1
Haris Saric <harissaric55@gmail.com>
2025-12-26 15:28:03 +0100

    Import fix

--- Dispatcher.Frontend/src/app/modules/admin/admin-module.ts
+++ Dispatcher.Frontend/src/app/modules/admin/admin-module.ts
@@ -22,6 +22,10 @@ import { TrucksComponent } from './vehicles/trucks/trucks.component';
 import { TruckFormModalComponent } from './vehicles/trucks/truck-form-modal/truck-form-modal.component';
 import { TrailersComponent } from './vehicles/trailers/trailers.component';
 import { TrailerFormModalComponent } from './vehicles/trailers/trailer-form-modal/trailer-form-modal.component';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 @NgModule({
   declarations: [
     ProductsComponent,
