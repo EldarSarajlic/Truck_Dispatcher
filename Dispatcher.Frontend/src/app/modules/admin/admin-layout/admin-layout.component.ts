aae773c65f5b6806efaeff39cc812ccc60589d01
Eldar Sarajlic <eldarsarajlic525@gmail.com>
2026-04-21 10:38:56 +0200

    Removed "Trailers" from sidebar, moved to "Vehicles" page

--- Dispatcher.Frontend/src/app/modules/admin/admin-layout/admin-layout.component.ts
+++ Dispatcher.Frontend/src/app/modules/admin/admin-layout/admin-layout.component.ts
@@ -12,7 +12,6 @@ export class AdminLayoutComponent {
     { label: 'ADMIN_PANEL.MENU.DASHBOARD', icon: 'ph-duotone ph-layout',        route: '/admin/dashboard' },
     { label: 'ADMIN_PANEL.MENU.USERS',     icon: 'ph-duotone ph-users-three',   route: '/admin/users'     },
     { label: 'ADMIN_PANEL.MENU.VEHICLES',  icon: 'ph-duotone ph-truck',         route: '/admin/vehicles'  },
-    { label: 'ADMIN_PANEL.MENU.TRAILERS',  icon: 'ph-duotone ph-truck-trailer', route: '/admin/trailers'  },
     { label: 'ADMIN_PANEL.MENU.INVENTORY', icon: 'ph-duotone ph-package',       route: '/admin/inventory' },
   ];
 }
